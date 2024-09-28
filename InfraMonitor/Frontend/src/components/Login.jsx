536134ce9ac5ff5c509e5f89d69c0db586f3b51c
Akshaj Paintola <akshajpaintola@gmail.com>
2024-09-28 15:17:14 +0530

    added check box to login page

--- InfraMonitor/Frontend/src/components/Login.jsx
+++ InfraMonitor/Frontend/src/components/Login.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -17,12 +18,11 @@ const Login = ({ onLogin }) => {
   return (
     <div className='bg-gradient-to-tr from-zinc-900 to-gray-700 flex justify-center h-screen items-center'>
       <div className='flex w-[70%] justify-center '>
-        <form className='border-2 border-white flex flex-col rounded-xl gap-3 px-16 pb-4 bg-gradient-to-r from-zinc-200 to-stone-300' onSubmit={handleLogin}>
+        <form className=' flex flex-col rounded-xl gap-3 px-16 pb-4 bg-gradient-to-r from-zinc-200 to-stone-300' onSubmit={handleLogin}>
           <h1 className='text-center font-bold text-4xl p-4 pt-6'>Login</h1>
           <div className='flex flex-col'>
             <label className='font-semibold' htmlFor="username">Username</label>
             <input
-            
               id="username"
               type="text"
               value={username}
@@ -35,18 +35,28 @@ const Login = ({ onLogin }) => {
             <label className='font-semibold' htmlFor="password">Password</label>
             <input
               id="password"
-              type="password"
+              type={showPassword ? "text" : "password"} // Toggle between text and password type
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
               className="px-2 py-1 rounded text-black bg-transparent border border-gray-600"
             />
+            <div className="flex items-center mt-2">
+              <input
+                type="checkbox"
+                id="showPassword"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)} // Toggle visibility
+                className="mr-2"
+              />
+              <label htmlFor="showPassword" className='text-sm'>Show Password</label>
+            </div>
           </div>
           <div className='flex justify-center items-center pb-4'>
             <button
-                type="submit"
-                className='mt-4 bg-gradient-to-tr from-zinc-900 to-gray-700 font-semibold  text-white p-2  rounded-xl hover:bg-gray-200 px-6'>
-                Continue
+              type="submit"
+              className='mt-4 bg-gradient-to-tr from-zinc-900 to-gray-700 font-semibold  text-white p-2 rounded-xl hover:bg-gray-200 px-6'>
+              Continue
             </button>
           </div>
         </form>
