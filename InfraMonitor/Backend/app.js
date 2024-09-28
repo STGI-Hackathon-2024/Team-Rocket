f0af81b1ec92d8a0d29fce5c80fa9cbe0d18b294
Akshaj Paintola <akshajpaintola@gmail.com>
2024-09-28 13:59:42 +0530

    added /login route

--- InfraMonitor/Backend/app.js
+++ InfraMonitor/Backend/app.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => {
     }
 );
 
+app.post('/login', (req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // using jwt for authentication
+    const token = jwt.sign({username}, 'secretkey', { expiresIn: '1h' });
+
+    res.json({
+        message: 'Authentication successful!',
+        token: token
+    });
+    }
+);
+
+
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
